c7863466faec677b8857d1704e5871bc2ebe3e1a
darth_vader <bozkayasalih01x@gmail.com>
2024-06-15 03:14:31 +0300

    update config

--- config.el
+++ config.el
@@ -6,13 +6,14 @@
 (setq doom-leader-key "C-x"
       doom-localleader-key "C-x")
 
-(setq doom-theme 'doom-one)
+(load-theme 'gruber-darker t)
 
 (setq display-line-numbers-type 'relative)
 
 (setq org-directory "~/org/")
 (after! evil
   ;; Define a function to simulate ESC
+
   (defun my-escape ()
     (interactive)
     (evil-force-normal-state))
@@ -21,46 +22,33 @@
   (define-key evil-visual-state-map "zz" 'my-escape)
   (define-key evil-insert-state-map "zz" 'my-escape))
 
+ (after! evil
+   (define-key evil-normal-state-map (kbd ":") #'execute-extended-command)
+  (define-key evil-visual-state-map (kbd ":") #'execute-extended-command))
+
+
+
+(ido-mode 1)
+(ido-everywhere 1)
+;;(ido-ubiquitous-mode 1)
+(setq evil-insert-state-cursor '(box "yellow")
+      evil-normal-state-cursor '(box "yellow"))
+
+(add-hook 'window-setup-hook #'toggle-frame-fullscreen)
+
+
 (after! dired
   (map! "C-x C-p" #'dired))
 
-(after! evil
-  (define-key evil-normal-state-map (kbd ":") #'execute-extended-command)
-  (define-key evil-visual-state-map (kbd ":") #'execute-extended-command))
+(map! "C-x C-p" #'evil-window-up
+        "C-x C-n" #'evil-window-down)
 
-(after! evil
-  (map! "C-x C-l" #'evil-window-up
-         "C-x C-h" #'evil-window-down))
+(map! "C-x C-]" #'split-window-vertically)
+(map! "C-x k" #'kill-current-buffer)
+(map! "C-x 0" #'doom/window-maximize-buffer)
 
+;; (global-set-key (kbd "M-x") 'smex)
+;; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; ;; This is your old M-x.
+;; (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
